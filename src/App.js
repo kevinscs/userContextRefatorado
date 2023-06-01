22d8621808f1945be760ee6ec74fce78eaf61ca4
Kevin Costa <kevinsatiro123@gmail.com>
2023-06-01 15:00:50 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import { useContext, useState, createContext } from 'react';
 import './App.css';
+//import P from 'prop-types';
+
+const globalState = {
+  title: 'O titulo do contexto',
+  body: 'O body do contexto',
+  counter: 0,
+};
+
+const GlobalContexte = createContext();
+
+
+// eslint-disable-next-line
+const Div = ({ children  }) => {
+  return (
+      <>
+        <H1 />
+        <P />
+      </>
+    ) 
+};
+
+// eslint-disable-next-line
+const H1 = () => {
+  const { contextState: {title, counter} } = useContext(GlobalContexte);
+  return <h1>{title} {counter}</h1>
+};
+
+// eslint-disable-next-line
+const P = () => {
+  const { contextState: {body, counter}, contextState, setContextState } = useContext(GlobalContexte);
+  return <p onClick={() => setContextState({ ...contextState, counter: counter + 1 })}>{body}</p>
+};
 
 function App() {
+  const [ contextState, setContextState ] = useState(globalState)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <GlobalContexte.Provider value={{ contextState, setContextState }}>
+      <Div />
+    </GlobalContexte.Provider>
+  )
 }
 
 export default App;
